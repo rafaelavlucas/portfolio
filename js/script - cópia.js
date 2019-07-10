384129891ba31a21b18624253191ff6ceb013659
rafaelalucas91 <rafaelalucas_91@hotmail.com>
2019-07-10 23:30:27 +0100

    UPD: css + js

---
+++ js/script - cópia.js
@@ -0,0 +1,76 @@
+//------ Script Open menu
+
+/* $( document ).ready(function() {
+ console.log( "ready!" );
+ $('#iconmenu').click(function() { 
+ $(this).toggleClass('iconopen iconclose'); 
+ $("#nav").toggleClass('navshow');                
+ });*/
+
+var iconMenu = document.querySelector('.mainnav- .iconmenu');
+
+iconMenu.addEventListener('click', openNav);
+
+function openNav() {
+    var iconOpen = document.querySelector('.iconmenu .iconopen');
+
+    iconOpen.classList.add('close');
+};
+
+
+
+/*
+//------ Script footer year
+
+var today = new Date();
+var year = today.getFullYear();
+document.querySelector('.year').innerHTML = year;
+
+
+//------ Script back to top         
+
+function runScroll() {
+    scrollTo(document.body, 0, 600);
+}
+var scrollme;
+scrollme = document.querySelector("#scrollme");
+scrollme.addEventListener("click", runScroll, false);
+
+function scrollTo(element, to, duration) {
+    if (duration <= 0) return;
+    var difference = to - element.scrollTop;
+    var perTick = difference / duration * 10;
+
+    setTimeout(function () {
+        element.scrollTop = element.scrollTop + perTick;
+        if (element.scrollTop == to) return;
+        scrollTo(element, to, duration - 10);
+    }, 10);
+
+
+});
+
+
+//------ tabs      
+
+function openTab(evt, tabName) {
+    // Declare all variables
+    var tabcontent;
+    var tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
--- js/script.js
+++ js/script.js
@@ -1,67 +1,26 @@
+window.onload = function (e) {
 
+    /* Open Menu bar */
 
-//------ Script Open menu
+    var iconMenu = document.querySelector('.iconmenu'),
+        iconOpen = document.querySelector('.iconopen'),
+        navBar = document.querySelector('.nav');
 
-        $( document ).ready(function() {
-        console.log( "ready!" );
-        $('#iconmenu').click(function() { 
-        $(this).toggleClass('iconopen iconclose'); 
-        $("#nav").toggleClass('navshow');                
-        });
-      
-         
-    //------ Script footer year
-   
-        var today = new Date(); 
-        var year = today.getFullYear();
-        document.querySelector('.year').innerHTML = year ; 
-             
-                 
-    //------ Script back to top         
-             
-        function runScroll() {
-          scrollTo(document.body, 0, 600);
+    iconMenu.addEventListener('click', openNav);
+
+    function openNav() {
+
+        if (iconOpen.classList.contains('close')) {
+            iconOpen.classList.remove('close');
+        } else {
+            iconOpen.classList.add('close');
         }
-        var scrollme;
-        scrollme = document.querySelector("#scrollme");
-        scrollme.addEventListener("click",runScroll,false)
 
-        function scrollTo(element, to, duration) {
-          if (duration <= 0) return;
-          var difference = to - element.scrollTop;
-          var perTick = difference / duration * 10;
-
-          setTimeout(function() {
-            element.scrollTop = element.scrollTop + perTick;
-            if (element.scrollTop == to) return;
-            scrollTo(element, to, duration - 10);
-          }, 10);
+        if (navBar.classList.contains('navshow')) {
+            navBar.classList.remove('navshow');
+        } else {
+            navBar.classList.add('navshow');
         }
-            
-             });
-          
+    };
 
-    //------ tabs      
-        
-        function openTab(evt, tabName) {
-    // Declare all variables
-    var tabcontent; 
-    var tablinks;
-
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (var i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
 }
-
