fb27d6cede2597e0138ad9235f0a5ae1e2b0cdcf
Rafaela <rafaelalucas_91@hotmail.com>
2019-07-24 14:43:16 +0100

    UPD: js

--- js/script.js
+++ js/script.js
@@ -3,22 +3,31 @@ window.onload = function (e) {
 
     /* Sticky Go Back */
     var goBack = document.querySelector('.go-back');
+
+    window.addEventListener('scroll', fixedGoBack)
     // calcular a altura do ecrã
-    window.onscroll = function () {
 
 
+    function fixedGoBack() {
+        var body = document.querySelector('body')
+        window.addEventListener('scroll', fixedGoBack)
         if (window.pageYOffset >= 190) {
-            goBack.classList.remove('bottom');
+
             goBack.classList.add('fixed');
 
         } else {
             goBack.classList.remove('fixed');
-            goBack.classList.add('bottom');
+
         }
 
-
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            goBack.classList.remove('fixed');
+            window.removeEventListener('scroll', fixedGoBack)
+        } else {
+            window.addEventListener('scroll', fixedGoBack)
+        }
+        console.log(document.querySelector('body').scrollHeight - document.querySelector('body').scrollTop, document.querySelector('body').clientHeight + 270)
     }
-
 
 
     /* Open Menu bar */
