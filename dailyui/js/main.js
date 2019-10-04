834c4aad1682ac4d83372a77eef1b7426232265c
rafaelalucas91 <rafaelalucas_91@hotmail.com>
2019-10-04 22:43:37 +0100

    upd: files

---
+++ dailyui/js/main.js
@@ -0,0 +1,23 @@
+window.onload = function (e) {
+
+    var dailyUi = 6;
+
+    function addItems(dailyUi) {
+        var itemContainer = document.getElementById("dailyui-items");
+        for (let i = 1; i <= dailyUi; i++) {
+
+            var template = `
+            <a href="/${i}" target="_blank">
+                
+                    <div class="item">
+                        <img src="assets/img-${i}.jpeg">
+                    </div>
+               
+            </a>`;
+            itemContainer.insertAdjacentHTML('beforeend', template);
+        }
+    }
+
+    addItems(dailyUi)
+
+}
