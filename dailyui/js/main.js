681ac91cf9d91a71e6c438a8b9307e2c14d9016f
rafaelalucas91 <rafaelalucas_91@hotmail.com>
2019-10-05 18:48:35 +0100

    upd: css + js

--- dailyui/js/main.js
+++ dailyui/js/main.js
@@ -1,22 +1,66 @@
 window.onload = function (e) {
 
-    var dailyUi = 6;
+    var nu = 1,
+        nm = 1,
+        im = 1;
 
-    function addItems(dailyUi) {
+
+
+    const items = [{
+            url: `${nu++}`,
+            name: `${nm++}`,
+            image: `assets/img-${im++}.jpeg`,
+            desc: "Sign Up"
+        },
+        {
+            url: `${nu++}`,
+            name: `${nm++}`,
+            image: `assets/img-${im++}.jpeg`,
+            desc: "Credit Card Checkout"
+        },
+        {
+            url: `${nu++}`,
+            name: `${nm++}`,
+            image: `assets/img-${im++}.jpeg`,
+            desc: "Landing Page"
+        },
+        {
+            url: `${nu++}`,
+            name: `${nm++}`,
+            image: `assets/img-${im++}.jpeg`,
+            desc: "teste"
+        },
+
+        {
+            url: `${nu++}`,
+            name: `${nm++}`,
+            image: `assets/img-${im++}.jpeg`,
+            desc: "laijito"
+        }
+    ];
+
+    function addItems() {
         var itemContainer = document.getElementById("dailyui-items");
-        for (let i = 1; i <= dailyUi; i++) {
+
+        for (let i = 0; i => items.length; i++) {
+            var name = items[i].name,
+                img = items[i].image,
+                desc = items[i].desc,
+                url = items[i].url;
 
             var template = `
-            <a href="${i}" target="_blank">
-                
-                    <div class="item">
-                        <img src="assets/img-${i}.jpeg">
-                    </div>
-               
+            <a href="${url}" target="_blank">
+                <div class="item">
+                    <article>
+                        <h3 class="name">${name}</h3>
+                        <p class="desc">${desc}</p>
+                    </article>
+                    <img src="${img}">
+                 </div>
             </a>`;
             itemContainer.insertAdjacentHTML('beforeend', template);
         }
     }
 
-    addItems(dailyUi)
+    addItems()
 }
