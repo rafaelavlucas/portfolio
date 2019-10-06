97f02ef35064315136f1addf3dcc28716f5ce1d8
rafaelalucas91 <rafaelalucas_91@hotmail.com>
2019-10-06 23:26:03 +0100

    upd: design

--- dailyui/js/main.js
+++ dailyui/js/main.js
@@ -1,42 +1,106 @@
 window.onload = function (e) {
 
+
+    // Sticky menu 
+
+    var menuNav = document.querySelector('nav .navigation');
+
+    window.onscroll = function () {
+
+        if (window.pageYOffset >= 70) {
+            menuNav.classList.add('sticky');
+
+        } else {
+            menuNav.classList.add('no-sticky');
+
+            setTimeout(function () {
+                menuNav.classList.remove('sticky');
+                menuNav.classList.remove('no-sticky');
+            }, 200);
+        }
+    }
+
+
+
     var nu = 1,
         nm = 1,
-        im = 1;
+        im = 1,
+        folder = "img";
 
 
 
     const items = [{
             url: `${nu++}`,
             name: `${nm++}`,
-            image: `assets/dailyui-${im++}.png`,
+            image: `${folder}/dailyui-${im++}.png`,
             desc: "Sign Up"
         },
         {
             url: `${nu++}`,
             name: `${nm++}`,
-            image: `assets/dailyui-${im++}.png`,
+            image: `${folder}/dailyui-${im++}.png`,
             desc: "Credit Card Checkout"
         },
         {
             url: `${nu++}`,
             name: `${nm++}`,
-            image: `assets/dailyui-${im++}.png`,
+            image: `${folder}/dailyui-${im++}.png`,
             desc: "Landing Page"
         },
         {
             url: `${nu++}`,
             name: `${nm++}`,
-            image: `assets/img-${im++}.jpeg`,
+            image: `${folder}/placeholder.jpg`,
             desc: "teste"
         },
-
         {
             url: `${nu++}`,
             name: `${nm++}`,
-            image: `assets/img-${im++}.jpeg`,
-            desc: "laijito"
-        }
+            image: `${folder}/placeholder.jpg`,
+            desc: "teste"
+        },
+        {
+            url: `${nu++}`,
+            name: `${nm++}`,
+            image: `${folder}/placeholder.jpg`,
+            desc: "teste"
+        },
+        {
+            url: `${nu++}`,
+            name: `${nm++}`,
+            image: `${folder}/placeholder.jpg`,
+            desc: "teste"
+        },
+        {
+            url: `${nu++}`,
+            name: `${nm++}`,
+            image: `${folder}/placeholder.jpg`,
+            desc: "teste"
+        },
+        {
+            url: `${nu++}`,
+            name: `${nm++}`,
+            image: `${folder}/placeholder.jpg`,
+            desc: "teste"
+        },
+        {
+            url: `${nu++}`,
+            name: `${nm++}`,
+            image: `${folder}/placeholder.jpg`,
+            desc: "teste"
+        },
+        {
+            url: `${nu++}`,
+            name: `${nm++}`,
+            image: `${folder}/placeholder.jpg`,
+            desc: "teste"
+        },
+        {
+            url: `${nu++}`,
+            name: `${nm++}`,
+            image: `${folder}/placeholder.jpg`,
+            desc: "teste"
+        },
     ];
 
     function addItems() {
@@ -55,7 +119,9 @@ window.onload = function (e) {
                         <h3 class="name">${name}</h3>
                         <p class="desc">${desc}</p>
                     </article>
-                    <img src="${img}">
+                    <div class="image"> 
+                        <img src="${img}">
+                    </div>
                  </div>
             </a>`;
             itemContainer.insertAdjacentHTML('beforeend', template);
@@ -63,4 +129,7 @@ window.onload = function (e) {
     }
 
     addItems()
+
+
+
 }
