16b435d1ee55495ddcb84614d7b18b3d1c910b5e
rafaelalucas91 <rafaelalucas_91@hotmail.com>
2019-10-10 23:14:55 +0100

    upd: calc css

--- dailyui/4/js/main.js
+++ dailyui/4/js/main.js
@@ -1,5 +1,19 @@
 window.addEventListener("load", event => {
 
+    var toggle = document.querySelector('.toggle'),
+        calculator = document.querySelector('.calculator');
 
+    toggle.addEventListener("click", changeMode);
+
+    function changeMode() {
+
+        if (toggle.classList.contains('off')) {
+            toggle.classList.remove('off');
+            calculator.setAttribute('data-theme', 'light')
+        } else {
+            toggle.classList.add('off');
+            calculator.setAttribute('data-theme', 'dark')
+        }
+    }
 
 });
