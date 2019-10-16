b2ef82bd5ea2110fa333b094469755cee551e9e5
rafaelalucas91 <rafaelavlucas@gmail.com>
2019-10-16 14:25:57 +0100

    upd: css + js

--- dailyui/5/js/main.js
+++ dailyui/5/js/main.js
@@ -1,5 +1,30 @@
 window.addEventListener("load", event => {
 
+    var container = document.querySelector('.container');
+
+    var theme = ["", "dark", "light"];
+
+
+    for (let i = 0; i < theme.length; i++) {
+
+
+        var template = `<div class="icon-app">
+<div class="icon" data-theme="${theme[i]}">
+    <div class="actions">
+        <span class="multiply"></span>
+        <span class="divide"></span>
+        <span class="minus"></span>
+        <span class="plus"></span>
+    </div>
+</div>
+</div>`;
+
+
+        container.insertAdjacentHTML('beforeend', template);
+
+    }
+
+
 
 
 
