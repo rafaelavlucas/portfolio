72829d2b5db340309c07112693f98caff0a95078
rafaelalucas91 <rafaelavlucas@gmail.com>
2019-12-18 18:33:40 +0000

    upd: daily ui 9

--- dailyui/9/js/main.js
+++ dailyui/9/js/main.js
@@ -5,13 +5,49 @@ window.addEventListener("load", event => {
     var iconTopExpand = document.querySelector('.icon.topExpand'),
         iconBottomExpand = document.querySelector('.icon.bottomExpand'),
         topContent = document.querySelector('.topContent'),
-        bottomContent = document.querySelector('.bottomContent');
+        bottomContent = document.querySelector('.bottomContent'),
+        playBtn = document.querySelector('.play');
 
+    // Songs 
+    let counter = 0;
+    const songs = [{
+            id: 0,
+            img: 'https://rafaelalucas91.github.io/assets/images/img-46.jpeg',
+            artistName: 'teste1',
+            songName: 'teste2',
+            song: '../songs/song1.mp3'
+        },
+        {
+            id: 1,
+            img: 'https://rafaelalucas91.github.io/assets/images/img-47.jpeg',
+            artistName: 'teste3',
+            songName: 'teste4',
+            song: '../songs/song2.mp3'
+        },
 
+        {
+            id: 2,
+            img: 'https://rafaelalucas91.github.io/assets/images/img-48.jpeg',
+            artistName: 'teste5',
+            songName: 'teste6',
+            song: '../songs/song3.mp3'
+        },
+    ];
 
     iconTopExpand.addEventListener("click", expandTop);
     iconBottomExpand.addEventListener("click", expandBottom);
+    playBtn.addEventListener("click", playSong);
 
+    function playSong() {
+
+        if (playBtn.classList.contains('pause')) {
+            playBtn.classList.remove('pause');
+            document.querySelector('.swiper-slide-active audio').pause();
+        } else {
+            playBtn.classList.add('pause');
+            document.querySelector('.swiper-slide-active audio').play();
+        }
+    }
 
     function expandTop() {
         if (topContent.classList.contains('expand')) {
@@ -27,6 +63,7 @@ window.addEventListener("load", event => {
                 draggable: true
             });
         }
+        albumSize();
     }
 
     function expandBottom() {
@@ -35,38 +72,36 @@ window.addEventListener("load", event => {
         } else {
             bottomContent.classList.add('expand');
         }
+        albumSize();
     }
 
-    // Songs 
-    let counter = 0;
-    const songs = [{
-            id: 0,
-            img: 'https://rafaelalucas91.github.io/assets/images/img-46.jpeg',
-            artistName: 'teste1',
-            songName: 'teste2'
-        },
-        {
-            id: 1,
-            img: 'https://rafaelalucas91.github.io/assets/images/img-47.jpeg',
-            artistName: 'teste3',
-            songName: 'teste4'
-        },
+    // change Album size
 
-        {
-            id: 2,
-            img: 'https://rafaelalucas91.github.io/assets/images/img-48.jpeg',
-            artistName: 'teste5',
-            songName: 'teste6'
-        },
-    ];
+    function albumSize() {
+
+        const albumContent = document.querySelectorAll('.albumContent');
+
+        if (topContent.classList.contains('expand') && bottomContent.classList.contains('expand')) {
+
+            albumContent.forEach(function (el) {
+                el.classList.add('small');
+            })
+        } else {
+            albumContent.forEach(function (el) {
+                el.classList.remove('small');
+            })
+        }
+    }
 
     var swipperWrapper = document.querySelector('.swiper-wrapper.slider-one');
     songs.forEach(function (el) {
         let template = `
         <div class="swiper-slide">
+        <audio src="${el.song}"></audio>
+        <div class="albumContent">
         <div class="albumCover"><img src="${el.img}"></div>
     <p class="artistName">${el.artistName}</p>
-    <p class="songName">${el.songName}</p></div>`;
+    <p class="songName">${el.songName}</p></div></div>`;
         swipperWrapper.insertAdjacentHTML('beforeend', template);
     })
 
@@ -76,7 +111,7 @@ window.addEventListener("load", event => {
         direction: "horizontal",
         loop: true,
         centeredSlides: true,
-        slidesPerView: 'auto',
+        slidesPerView: 1,
         spaceBetween: 20,
         speed: 800,
         allowTouchMove: false,
@@ -94,17 +129,33 @@ window.addEventListener("load", event => {
         }
     });
 
-
     // Album BG
     const imageBg = document.querySelector('.imageBg'),
         next = document.querySelector('.swiper-button-next'),
         prev = document.querySelector('.swiper-button-prev');
 
-    next.addEventListener("click", changeBg);
-    prev.addEventListener("click", changeBg);
+    next.addEventListener("click", changeSong);
+    prev.addEventListener("click", changeSong);
+
+    function changeSong() {
+
+        if (playBtn.classList.contains('pause')) {
+            document.querySelectorAll('audio').forEach(function (el) {
+                el.pause()
+            });
+            document.querySelector('.swiper-slide-active audio').play();
+        }
+        changeBg();
+
+    }
 
     function changeBg() {
         imageBg.src = document.querySelector('.slider-one .swiper-slide-active img').src;
+        imageBg.classList.add('animeBg');
+
+        setTimeout(() => {
+            imageBg.classList.remove('animeBg');
+        }, 800);
     }
 
     changeBg();
@@ -114,20 +165,20 @@ window.addEventListener("load", event => {
     const musicGroups = document.querySelector('.swiper-wrapper.slider-two'),
 
         groups = [{
-                title: "teste",
+                title: "Songs",
+                img: 'https://rafaelalucas91.github.io/assets/images/img-19.jpeg'
+            },
+            {
+                title: 'Artists',
+                img: 'https://rafaelalucas91.github.io/assets/images/img-20.jpeg'
+            },
+            {
+                title: 'Playlists',
                 img: 'https://rafaelalucas91.github.io/assets/images/img-18.jpeg'
             },
             {
-                title: 'teste',
-                img: 'https://rafaelalucas91.github.io/assets/images/img-18.jpeg'
-            },
-            {
-                title: 'teste',
-                img: 'https://rafaelalucas91.github.io/assets/images/img-18.jpeg'
-            },
-            {
-                title: 'teste',
-                img: 'https://rafaelalucas91.github.io/assets/images/img-18.jpeg'
+                title: 'Favourites',
+                img: 'https://rafaelalucas91.github.io/assets/images/img-30.jpeg'
             }
         ];
 
@@ -140,12 +191,16 @@ window.addEventListener("load", event => {
 
         var template2 =
             `<div class="swiper-slide group">
-            <img src="${img}">
+            <figure>
+                <img src="${img}">
+            </figure>
             <p class="titleGroup">${title}</p>
         </div>`;
 
         musicGroups.insertAdjacentHTML('beforeend', template2);
     }
+
+
 
 
 
