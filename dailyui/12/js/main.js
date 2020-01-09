87c10c153ae0f6907f642b4add9db85fefa37713
rafaelalucas91 <rafaelalucas_91@hotmail.com>
2020-01-09 23:19:52 +0000

    upd: css + js

--- dailyui/12/js/main.js
+++ dailyui/12/js/main.js
@@ -24,19 +24,45 @@ window.addEventListener("load", event => {
 
     ];
 
+    const btn = document.querySelector('.btn'),
+        itemNumber = document.querySelector('.itemNumber'),
+        inputQuantity = document.querySelector('.inputQuantity'),
+        plus = document.querySelector('.plus'),
+        minus = document.querySelector('.minus');
 
     ////////////////
     // Events
     ////////////////
 
 
+    btn.addEventListener('click', addItem);
 
-
-
+    plus.addEventListener("click", plusQuantity);
+    minus.addEventListener("click", minusQuantity);
 
     ////////////////
     // Functions
     //////////////// 
+
+
+    function plusQuantity() {
+        if (inputQuantity.value < 5) {
+            inputQuantity.value == inputQuantity.value++;
+        }
+    }
+
+    function minusQuantity() {
+        if (inputQuantity.value > 1) {
+            inputQuantity.value == inputQuantity.value--;
+        }
+    }
+
+
+    function addItem() {
+        itemNumber.style.display = "flex";
+        itemNumber.innerHTML = inputQuantity.value;
+
+    }
 
     // Populate the images for Swiper
 
@@ -57,16 +83,16 @@ window.addEventListener("load", event => {
     // Make the slider function
 
     var galleryThumbs = new Swiper('.galleryThumbs', {
-        spaceBetween: 10,
+        spaceBetween: 0,
         slidesPerView: 'auto',
-        loop: true,
-        freeMode: true,
-        loopedSlides: 5, //looped slides should be the same
-        watchSlidesVisibility: true,
-        watchSlidesProgress: true,
+        loop: false,
+        allowTouchMove: false,
+        allowSlidePrev: false,
+        allowSlideNext: false,
+
     });
     var galleryMain = new Swiper('.galleryMain', {
-        spaceBetween: 0,
+        spaceBetween: 20,
         slidesPerView: 'auto',
         loop: true,
         loopedSlides: 5, //looped slides should be the same
