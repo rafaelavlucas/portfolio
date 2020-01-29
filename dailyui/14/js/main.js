b623a6079dbf5f8b2f108f65afae827203035db1
rafaelalucas91 <rafaelalucas_91@hotmail.com>
2020-01-29 18:33:20 +0000

    upd js

--- dailyui/14/js/main.js
+++ dailyui/14/js/main.js
@@ -5,24 +5,27 @@ window.addEventListener("load", event => {
         hour = minute * 60,
         day = hour * 24;
 
+    var today = new Date();
+    var newdate = new Date();
 
 
-    let countDown = new Date('Mar 30, 2020 00:00:00').getTime(),
-        x = setInterval(function () {
 
-            let now = new Date().getTime(),
-                distance = countDown - now;
+    setTimeout(() => {
+        let countDown = new Date(newdate.setDate(today.getDate() + 60)).getTime(),
+            x = setInterval(function () {
 
+                let now = new Date().getTime(),
+                    distance = countDown - now;
 
-            setTimeout(() => {
                 document.getElementById('days').innerText = Math.floor(distance / (day)),
                     document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
                     document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
                     document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-            }, 500);
+
+            }, second)
 
 
-        }, second)
+    }, 500);
 
     if (window.innerWidth < 799) {
         document.querySelector('.wrapper').style.height = window.innerHeight + "px";
