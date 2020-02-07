fc09691a11a102e008c49482ad11cbe42a11e5da
rafaelalucas91 <rafaelalucas_91@hotmail.com>
2020-02-07 18:33:21 +0000

    upd: daily ui 16

--- dailyui/16/js/main.js
+++ dailyui/16/js/main.js
@@ -1,24 +1,5 @@
 window.addEventListener("load", event => {
 
-    const toggle = document.querySelector('.toggle'),
-        wrapper = document.querySelector('.wrapper');
 
-    toggle.addEventListener("click", addOn);
-
-    function addOn() {
-        if (toggle.classList.contains("on")) {
-            toggle.classList.remove("on");
-            toggle.dataset.theme = "";
-            wrapper.classList.remove("active");
-        } else {
-
-            wrapper.classList.add("active");
-            setTimeout(() => {
-                toggle.dataset.theme = "on";
-                toggle.classList.add("on");
-            }, 100);
-        }
-
-    };
 
 });
