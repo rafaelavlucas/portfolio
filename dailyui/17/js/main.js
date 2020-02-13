9dd3c90a92c3c4dd3276248b704fe1740ccf2f48
rafaelalucas91 <rafaelalucas_91@hotmail.com>
2020-02-13 18:30:18 +0000

    upd: css

--- dailyui/17/js/main.js
+++ dailyui/17/js/main.js
@@ -1,4 +1,14 @@
 window.addEventListener("load", event => {
 
+    const cta = document.querySelector('.cta'),
+        container = document.querySelector('.container');
+
+
+    cta.addEventListener("click", printReceipt);
+
+    function printReceipt() {
+        container.classList.add('anime');
+        cta.innerHTML = "";
+    }
 
 });
