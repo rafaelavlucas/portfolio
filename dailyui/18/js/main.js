3749d0edfe6a0d7796d58790acfbc1c75ef7edb1
rafaelalucas91 <rafaelalucas_91@hotmail.com>
2020-03-23 22:15:31 +0000

    upd: css

--- dailyui/18/js/main.js
+++ dailyui/18/js/main.js
@@ -1,31 +1,67 @@
 window.addEventListener("load", event => {
 
+    var d = new Date();
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+    var day = d.getDate();
+    var today = days[d.getDay()] + ", " + months[d.getMonth()] + " ";
+    var cenas = 1;
+    var getPrevDay = days[d.getDay()] + ", " + months[d.getMonth()] + " ";
 
     const data = [{
-        day: "teste1",
-        steps: "3604",
-        time: "1.14",
-        distance: "2.14",
-        calories: "57",
-    }, {
-        day: "teste2",
-        steps: "300",
-        time: "3",
-        distance: "jvn",
-        calories: "200",
-    }, {
-        day: "teste3",
-        steps: "300",
-        time: "fhrjkevn",
-        distance: "jvn",
-        calories: "200",
-    }];
+            day: today + day,
+            steps: "4414",
+            time: "0.54",
+            distance: "2.74",
+            calories: "68",
 
+        }, {
+            day: today + (day - 1),
+            steps: "5682",
+            time: "1.14",
+            distance: "3.38",
+            calories: "86",
+        }, {
+            day: today + (day - 2),
+            steps: "24404",
+            time: "2.40",
+            distance: "9.05",
+            calories: "222",
+        }, {
+            day: today + (day - 3),
+            steps: "7770",
+            time: "1.44",
+            distance: "4.67",
+            calories: "117",
+        }, {
+            day: today + (day - 4),
+            steps: "10599",
+            time: "2.12",
+            distance: "6.27",
+            calories: "156",
+        },
+        {
+            day: today + (day - 5),
+            steps: "11161",
+            time: "1.52",
+            distance: "7.26",
+            calories: "181",
+        }, {
+            day: today + (day - 6),
+            steps: "3740",
+            time: "0.46",
+            distance: "2.23",
+            calories: "57",
+        }
+    ];
 
-    const stepsValue = 8000,
-        timeValue = 4,
-        distanceValue = 10,
-        caloriesValue = 400;
+    const stepsValue = 15000,
+        timeValue = 3,
+        distanceValue = 5,
+        caloriesValue = 200;
 
     const icons = ["assets/iconSteps.svg", "assets/iconTime.svg", "assets/iconDistance.svg", "assets/iconCalories.svg"];
 
@@ -36,13 +72,13 @@ window.addEventListener("load", event => {
         <div class="swiper-slide"> 
             <div class="fitness">
                 <div class="fitness__top">
-                    <p class="fitness__desc">today</p>
+                    
                     <p class="fitness__date">${el.day}</p>
                 </div>
 
             <div class="fitness__content">
                 <div class="fitness__square steps">
-                 <span class="icon"></span>
+                 <span class="iconBig"></span>
                     <p class="fitness__name">${el.steps}</p>
                     <p class="fitness__desc">Steps</p>  
                     <div class="fitness__bar">
@@ -50,7 +86,7 @@ window.addEventListener("load", event => {
                    </div>
                 </div>
                 <div class="fitness__square time">
-                <span class="icon"></span>
+                <span class="iconBig"></span>
                     <p class="fitness__name">${el.time}</p>
                     <p class="fitness__desc">Time</p>
                     <p class="fitness__small">hours</p>
@@ -59,7 +95,7 @@ window.addEventListener("load", event => {
                     </div>
                 </div>
                 <div class="fitness__square distance">
-                <span class="icon"></span>
+                <span class="iconBig"></span>
                     <p class="fitness__name">${el.distance}</p>
                     <p class="fitness__desc">Distance</p>
                     <p class="fitness__small">Km</p>
@@ -68,7 +104,7 @@ window.addEventListener("load", event => {
                     </div>
                 </div>
                 <div class="fitness__square calories">
-                <span class="icon"></span>
+                <span class="iconBig"></span>
                     <p class="fitness__name">${el.calories}</p>
                     <p class="fitness__desc">burned</p>
                     <p class="fitness__small">Kcal</p>
@@ -82,6 +118,8 @@ window.addEventListener("load", event => {
         swiperWrapper.insertAdjacentHTML("beforeend", template);
 
     });
+
+
 
 
     var mySwiper = new Swiper('.swiper-container', {
@@ -111,6 +149,20 @@ window.addEventListener("load", event => {
     if (window.innerWidth <= 460) {
         document.querySelector('.container').style.height = window.innerHeight + "px";
         document.querySelector('.wrapper').style.height = window.innerHeight + "px";
+    }
+
+
+
+
+    for (let i = 0; i < data.length; i++) {
+
+        const {
+            steps
+        } = data[i]
+
+        var height = steps * 100 / stepsValue;
+
+        document.querySelectorAll(".swiper-pagination-bullet")[i].style.height = height + "%";
     }
 
 
