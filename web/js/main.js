f208d47cc25b3c628a9ccd86955020f5492d0eb2
rafaelavlucas <rafaelalucas_91@hotmail.com>
2020-09-03 20:52:24 +0100

    upd: css

--- web/js/main.js
+++ web/js/main.js
@@ -1,62 +1,62 @@
 window.onload = function (e) {
 
-
-    function splitTitle() {
-        const titleWord = document.querySelector(".mainHeading__title"),
-            titleSplit = titleWord.innerText.split("");
-
-
-        // Title Split Characters
-        setTimeout(() => {
-            titleWord.innerHTML = "";
-            titleSplit.forEach(function (el) {
-                let template = `
-              <span>${el}</span>`;
-                titleWord.style.opacity = "1";
-                titleWord.insertAdjacentHTML("beforeend", template);
-            });
-        }, 300);
-    }
-
-
     /*
-        function splitDescription() {
-            const descriptionWord = document.querySelector(".mainHeading__description--one"),
-                descriptionSplit = descriptionWord.innerText.split("");
+        function splitTitle() {
+            const titleWord = document.querySelector(".mainHeading__title"),
+                titleSplit = titleWord.innerText.split("");
 
-            //Split Characters
 
+            // Title Split Characters
             setTimeout(() => {
-                descriptionWord.innerHTML = "";
-                descriptionSplit.forEach(function (el) {
+                titleWord.innerHTML = "";
+                titleSplit.forEach(function (el) {
                     let template = `
                   <span>${el}</span>`;
-                    descriptionWord.style.opacity = "1";
-                    descriptionWord.insertAdjacentHTML("beforeend", template);
+                    titleWord.style.opacity = "1";
+                    titleWord.insertAdjacentHTML("beforeend", template);
                 });
-            }, 1000);
-
+            }, 300);
         }
 
-        function splitDescription2() {
-            const descriptionWord2 = document.querySelector(".mainHeading__description--two"),
-                descriptionSplit2 = descriptionWord2.innerText.split("");
 
-            //Split Characters
-            setTimeout(() => {
-                descriptionWord2.innerHTML = "";
-                descriptionSplit2.forEach(function (el) {
-                    let template = `
-                  <span>${el}</span>`;
-                    descriptionWord2.style.opacity = "1";
-                    descriptionWord2.insertAdjacentHTML("beforeend", template);
-                });
-            }, 1300);
-        }
+        
+            function splitDescription() {
+                const descriptionWord = document.querySelector(".mainHeading__description--one"),
+                    descriptionSplit = descriptionWord.innerText.split("");
 
-          splitDescription();
-        splitDescription2();
-    */
+                //Split Characters
+
+                setTimeout(() => {
+                    descriptionWord.innerHTML = "";
+                    descriptionSplit.forEach(function (el) {
+                        let template = `
+                      <span>${el}</span>`;
+                        descriptionWord.style.opacity = "1";
+                        descriptionWord.insertAdjacentHTML("beforeend", template);
+                    });
+                }, 1000);
+
+            }
+
+            function splitDescription2() {
+                const descriptionWord2 = document.querySelector(".mainHeading__description--two"),
+                    descriptionSplit2 = descriptionWord2.innerText.split("");
+
+                //Split Characters
+                setTimeout(() => {
+                    descriptionWord2.innerHTML = "";
+                    descriptionSplit2.forEach(function (el) {
+                        let template = `
+                      <span>${el}</span>`;
+                        descriptionWord2.style.opacity = "1";
+                        descriptionWord2.insertAdjacentHTML("beforeend", template);
+                    });
+                }, 1300);
+            }
+
+              splitDescription();
+            splitDescription2();
+        */
 
     splitTitle();
 
